7aba2623369268f0f5c7eb2d5a5bfa8ae134c00a
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-Air.fibertel.com.ar>
2024-10-15 22:59:32 -0300

    fixes posters

--- src/sections/TopPosters.tsx
+++ src/sections/TopPosters.tsx
@@ -3,7 +3,10 @@ export default function TopPosters() {
   return (
     <section className="container mx-auto space-y-5" id="top">
       <h2 className="text-2xl font-semibold px-4 lg:text-4xl text-center text-white">Top Posters</h2>
-      <p className="italic text-center text-neutral-400 text-sm">*scroll horizontally to see more posters</p>
+      <div>
+        <p className="italic text-center text-neutral-400 text-sm">*scroll horizontally to see more posters</p>
+        <p className="italic text-center text-neutral-400 text-sm lg:hidden">*click on the poster to flip it</p>
+      </div>
       <PosterCarrousel />
     </section>
   )
